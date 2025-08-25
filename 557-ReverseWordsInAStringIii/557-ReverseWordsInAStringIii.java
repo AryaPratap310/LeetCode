1e6cce8dd12cbb541e7ad4c066000a31fc10c07d
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:20:07 +0530

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.java
@@ -0,0 +1,26 @@
+// Last updated: 25/08/2025, 17:20:06
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<words.length; i++){
+            sb.append(reverse(words[i]));
+            if(i < words.length-1) sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    public String reverse(String word){
+        char[] ch = word.toCharArray();
+        int start = 0;
+        int end = ch.length-1;
+        while(start < end){
+            char temp = ch[start];
+            ch[start] = ch[end];
+            ch[end] = temp;
+            start++;
+            end--;
+        }
+        return String.valueOf(ch);
+    }
+}
