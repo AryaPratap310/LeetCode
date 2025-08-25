b4ccb27fc9db5c70f3cc77ba33e1e8998b5556f1
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:20:03 +0530

    Create 575-DistributeCandies

---
+++ 575-DistributeCandies/575-DistributeCandies.java
@@ -0,0 +1,17 @@
+// Last updated: 25/08/2025, 17:20:02
+class Solution {
+    public int distributeCandies(int[] candyType) {
+       HashSet<Integer>res = new HashSet<>();
+        int candytype = 0 ;
+        for(int n : candyType){
+            if(!res.contains(n)) {
+                candytype++;
+                res.add(n);
+            }
+        }
+        int allowed = candyType.length / 2;
+        return Math.min(candytype , allowed); 
+        
+
+    }
+}
