d72204ccab9aed76bc8b93f79219bd8142187339
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:16:33 +0530

    Create 1975-MinimumDistanceToTheTargetElement

---
+++ 1975-MinimumDistanceToTheTargetElement/1975-MinimumDistanceToTheTargetElement.java
@@ -0,0 +1,12 @@
+// Last updated: 25/08/2025, 17:16:32
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                ans = Math.min(ans, Math.abs(i - start));
+            }
+        }
+        return ans;
+    }
+}
