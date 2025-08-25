8862661a981740eac4fa0092cb66018c93df1065
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:21:14 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,49 @@
+// Last updated: 25/08/2025, 17:21:13
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        // method  1
+        int arr[]=new int[26];
+        for(char ch:s.toCharArray()){
+            arr[ch-'a']++;
+        }
+        for(char ch:t.toCharArray()){
+            arr[ch-'a']--;
+        }
+
+        for(int x:arr){
+            if(x!=0){
+                return false;
+            }
+        }
+        return true;
+
+
+        // method  2
+
+        // if(s.length()!=t.length()){
+        //     return false;
+        // }
+        // HashMap<Character,Integer> check=new HashMap<>();
+        // for(int i=0;i<s.length();i++){
+        //     check.put(s.charAt(i),check.getOrDefault(s.charAt(i),0)+1);
+        // }
+
+        // for(int i=0;i<t.length();i++){
+        //     if(!check.containsKey(t.charAt(i)) || check.get(t.charAt(i))==0){
+        //         return false;
+        //     }
+        //     check.put(t.charAt(i),check.get(t.charAt(i))-1);
+        // }
+        // return true;
+
+        // method  3
+
+        // char[] sChars = s.toCharArray();
+        // char[] tChars = t.toCharArray();
+        
+        // Arrays.sort(sChars);
+        // Arrays.sort(tChars);
+        
+        // return Arrays.equals(sChars, tChars);  
+    }
+}
