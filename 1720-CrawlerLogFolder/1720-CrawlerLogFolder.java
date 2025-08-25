f30c1c50729128026a2894ed3565d3513344e99f
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:17:19 +0530

    Create 1720-CrawlerLogFolder

---
+++ 1720-CrawlerLogFolder/1720-CrawlerLogFolder.java
@@ -0,0 +1,18 @@
+// Last updated: 25/08/2025, 17:17:18
+class Solution {
+    public int minOperations(String[] logs) {
+        Stack<String> paths_stack = new Stack<>();
+
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (!paths_stack.isEmpty()) {
+                    paths_stack.pop();
+                }
+            } else if (!log.equals("./")) {
+                paths_stack.push(log);
+            }
+        }
+
+        return paths_stack.size();
+    }
+}
