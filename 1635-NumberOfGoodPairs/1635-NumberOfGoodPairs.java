4f4fdc2148e7093771df1491d7fe59273b52f52a
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:17:33 +0530

    Create 1635-NumberOfGoodPairs

---
+++ 1635-NumberOfGoodPairs/1635-NumberOfGoodPairs.java
@@ -0,0 +1,18 @@
+// Last updated: 25/08/2025, 17:17:32
+class Solution {
+    public int numIdenticalPairs(int[] guestList) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        int ans = 0;
+        
+        for(int friend:guestList)
+        {
+            int friendCount = hm.getOrDefault(friend,0);
+            ans+=friendCount;
+            hm.put(friend,friendCount+1);
+        }
+        
+        
+        return ans;
+    }
+}
