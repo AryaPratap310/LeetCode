94db9243c444faf068d34310c83aaad3aca50439
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:16:09 +0530

    Create 2177-CheckWhetherTwoStringsAreAlmostEquivalent

---
+++ 2177-CheckWhetherTwoStringsAreAlmostEquivalent/2177-CheckWhetherTwoStringsAreAlmostEquivalent.java
@@ -0,0 +1,16 @@
+// Last updated: 25/08/2025, 17:16:08
+class Solution { 
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+        Map<Character,Integer> map = new HashMap();
+        for (int i = 0; i < word1.length(); i++) {
+            map.put(word1.charAt(i), map.getOrDefault(word1.charAt(i), 0) + 1);
+            map.put(word2.charAt(i), map.getOrDefault(word2.charAt(i), 0) - 1);
+        }
+        for (int i : map.values()) { //get value set
+            if (i > 3 || i < -3) { 
+                return false;
+            }
+        }
+        return true;
+    }
+}
