01c4c7e2755003da5fd335de1bcd5aaa04ef4826
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:16:43 +0530

    Create 1920-DetermineColorOfAChessboardSquare

---
+++ 1920-DetermineColorOfAChessboardSquare/1920-DetermineColorOfAChessboardSquare.java
@@ -0,0 +1,16 @@
+// Last updated: 25/08/2025, 17:16:43
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        char letter = coordinates.charAt(0);
+        int digit = coordinates.charAt(1) - '0';
+        
+        
+        if(letter == 'a' || letter == 'c' || letter == 'e' || letter == 'g'){
+			// when digit is even
+            return digit % 2 == 0;
+        }else{
+		   //when digit is odd
+            return digit % 2 == 1;
+        }
+    }
+}
