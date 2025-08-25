868366b2913c80ec2c7e18481ddb422fb571026e
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:17:23 +0530

    Create 1693-SumOfAllOddLengthSubarrays

---
+++ 1693-SumOfAllOddLengthSubarrays/1693-SumOfAllOddLengthSubarrays.java
@@ -0,0 +1,18 @@
+// Last updated: 25/08/2025, 17:17:23
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int result = 0;
+        int n = arr.length;
+        for(int i=0; i<n ;i++){
+            int end = i+1;
+            int start = n-i;
+            int total=start*end;
+            int odd=total/2;
+            if(total%2==1){
+                odd++;
+            }
+            result+=odd*arr[i];
+        }
+        return result;
+    }
+}
