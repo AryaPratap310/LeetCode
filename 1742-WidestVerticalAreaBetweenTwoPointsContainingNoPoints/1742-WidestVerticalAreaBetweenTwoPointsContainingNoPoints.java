6035313d9510869937e6cad99d6b0d05525e5178
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:17:14 +0530

    Create 1742-WidestVerticalAreaBetweenTwoPointsContainingNoPoints

---
+++ 1742-WidestVerticalAreaBetweenTwoPointsContainingNoPoints/1742-WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
@@ -0,0 +1,13 @@
+// Last updated: 25/08/2025, 17:17:14
+class Solution {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        int[] xSorted = Arrays.stream(points).mapToInt(point -> point[0]).sorted().toArray();
+
+        int maxWidth = 0;
+        for (int i = 0; i < xSorted.length - 1; i++) {
+            maxWidth = Math.max(maxWidth, xSorted[i + 1] - xSorted[i]);
+        }
+
+        return maxWidth;        
+    }
+}
