647b343107c7f48f43fc7169d0125afaad5dbfb5
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:15:45 +0530

    Create 2401-CountAsterisks

---
+++ 2401-CountAsterisks/2401-CountAsterisks.java
@@ -0,0 +1,13 @@
+// Last updated: 25/08/2025, 17:15:44
+class Solution {
+       public int countAsterisks(String s) {
+        int res = 0, bars = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '*' && bars % 2 == 0)
+                res++;
+            if (s.charAt(i) == '|')
+                bars++;
+        }
+        return res;
+    }
+}
