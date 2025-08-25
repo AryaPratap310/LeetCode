2d2758bce094e106d533f3b99e42dcd0d15a7d53
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:16:03 +0530

    Create 2226-RingsAndRods

---
+++ 2226-RingsAndRods/2226-RingsAndRods.java
@@ -0,0 +1,16 @@
+// Last updated: 25/08/2025, 17:16:02
+class Solution {
+    public int countPoints(String rings) {
+        int rods[] = new int[10];
+        
+        // Traverse and turn bit corresponding to the character found
+        // in the respective ring
+        for(int i = 0; i < rings.length(); i+=2) 
+            rods[rings.charAt(i+1) - '0'] |= (1<<(rings.charAt(i) - 'A'));
+        
+        // Count rods with all 3 bits turned on
+        int count = 0;
+        for(int i : rods) if(i == 131138) count++;
+        return count;
+    }
+}
