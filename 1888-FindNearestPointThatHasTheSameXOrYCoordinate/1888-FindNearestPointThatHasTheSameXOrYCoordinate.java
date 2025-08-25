a45e19d9de79819fcafb2b60155f7c71066be9f2
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:16:52 +0530

    Create 1888-FindNearestPointThatHasTheSameXOrYCoordinate

---
+++ 1888-FindNearestPointThatHasTheSameXOrYCoordinate/1888-FindNearestPointThatHasTheSameXOrYCoordinate.java
@@ -0,0 +1,20 @@
+// Last updated: 25/08/2025, 17:16:51
+class Solution {
+    public int nearestValidPoint(int x, int y, int[][] points) {
+        int ans=-1;
+        int temp=10001;
+        for(int i=0;i<points.length;i++)
+        {
+            if(points[i][0]==x||points[i][1]==y)
+            {
+                int val=Math.abs(points[i][0]-x)+Math.abs(points[i][1]-y);
+                if(val<temp){
+                    temp=val;
+                    ans=i;
+                }
+            }
+        }
+        return ans;
+
+    }
+}
