27230fdf5a286efcc7f60c0c57d56db81ee5aad0
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:17:26 +0530

    Create 1682-MostVisitedSectorInACircularTrack

---
+++ 1682-MostVisitedSectorInACircularTrack/1682-MostVisitedSectorInACircularTrack.java
@@ -0,0 +1,15 @@
+// Last updated: 25/08/2025, 17:17:25
+class Solution {
+    public List<Integer> mostVisited(int n, int[] rounds) {
+        var start = rounds[0];
+        var end = rounds[rounds.length - 1];   
+        var res = new ArrayList<Integer>();
+        if (start <= end) {
+            for (int i = start; i <= end; i++) res.add(i);
+        } else {
+            for (int i = 1; i <= end; i++) res.add(i);
+            for (int i = start; i <= n; i++) res.add(i);
+        }
+        return res;
+    }
+}
