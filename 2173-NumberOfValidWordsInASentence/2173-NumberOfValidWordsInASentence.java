3152a7c03868036072742bb0bb38626745bbaa61
AryaPratap310 <offarya1@gmail.com>
2025-08-25 17:16:10 +0530

    Create 2173-NumberOfValidWordsInASentence

---
+++ 2173-NumberOfValidWordsInASentence/2173-NumberOfValidWordsInASentence.java
@@ -0,0 +1,18 @@
+// Last updated: 25/08/2025, 17:16:10
+class Solution {
+    public int countValidWords(String sentence) {
+        String regex = "^([a-z]+(-?[a-z]+)?)?(!|\\.|,)?$";
+        String r2 = "[^0-9]+";
+        String[] arr = sentence.split("\\s+");
+        int ans = 0;
+        for(String s: arr)
+        {
+            if(s.matches(regex) && s.matches(r2))
+            {
+                ans++;
+                //System.out.println(s);
+            }
+        }
+        return ans;
+    }
+}
